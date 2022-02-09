86644f052cbf5234f22f4c69e2cd3bad1f87ea4e
jayeshmanglani <jayesh97manglani@gmail.com>
2022-02-10 01:06:59 +0530

    My new java project for practice.

---
+++ Practice/src/basics/NamingConventions.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class NamingConventions {
+
+	public static void main(String[] args)
+	{
+		//Our classes will be named in PascalCOnvention
+		//Our methods will be named in camelConventions
+		System.out.println("Hello World!");
+
+	}
+
+}
